4630a5b500743082de7eea0be73c3c0cb0bf6ccb
Abreu <felipe.abreu@accenture.com>
2020-02-19 13:13:42 -0300

    se aplicado, este commit irá implementar o layout inicial do aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,29 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Container(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Lista de tarefas"),
+        ),
+        body: Container(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.purple,
+          foregroundColor: Colors.white,
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: null,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
